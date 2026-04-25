96161feaa54151b2200fc5d0acac197ad0821fb2
bushra-batool-codes <batool3024@gmail.com>
2026-04-25 19:32:33 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,10 @@
-
+def validate_patient(pid, name, age, gender):
+    if not pid:
+        return False, "Invalid ID"
+    if not name.strip():
+        return False, "Name required"
+    if age <= 0:
+        return False, "Invalid age"
+    if gender.lower() not in ['male', 'female', 'other']:
+        return False, "Invalid gender"
+    return True, "Valid"
